bb02cdf8ad3af55c86d7d0d7b7c2f2cfd5f0a883
Gonzalo <gonzalo.08.ge@gmail.com>
2023-02-08 23:05:50 -0300

    agregue ejercicio d

--- evaluacion.js
+++ evaluacion.js
@@ -1,7 +1,7 @@
 //Ejercicio a
 let nombre = prompt("Ingrese su nombre: ")
 document.write(`Bienvenido ${nombre}`)
-Ejercicio c
+// Ejercicio c
 let nro1 = prompt("Ingrese un numero A: ")
 let nro2 = prompt("Ingrese un numero B: ")
 if (nro1>nro2) {
@@ -9,4 +9,14 @@ if (nro1>nro2) {
 }
 else{
     alert(`El numero B(${nro2}) es mayor`)
+}
+//Ejercicio d
+let a = prompt("Ingrese un numero A: ")
+let b = prompt("Ingrese un numero B: ")
+let c = prompt("Ingrese un numero C: ")
+if (a == b && a == c && b == c){
+    document.write("Los 3 numeros son iguales")
+}
+else{
+    document.write("Los 3 numeros no son iguales")
 }
