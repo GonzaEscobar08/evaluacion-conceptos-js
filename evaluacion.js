52a473b39fa3645eff8cf5732594ffb8a49fcbef
Gonzalo <gonzalo.08.ge@gmail.com>
2023-02-08 23:04:44 -0300

    agregue ejercicio c

--- evaluacion.js
+++ evaluacion.js
@@ -1,3 +1,12 @@
 //Ejercicio a
 let nombre = prompt("Ingrese su nombre: ")
 document.write(`Bienvenido ${nombre}`)
+Ejercicio c
+let nro1 = prompt("Ingrese un numero A: ")
+let nro2 = prompt("Ingrese un numero B: ")
+if (nro1>nro2) {
+    alert(`El numero A(${nro1}) es mayor`)
+}
+else{
+    alert(`El numero B(${nro2}) es mayor`)
+}
